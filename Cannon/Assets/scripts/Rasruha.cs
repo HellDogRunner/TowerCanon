519d4e097610d3f5cb05f21bd7e988a16122e4e8
HellDogRunner <monolito159@mail.ru>
2022-03-24 19:28:24 +0300

    V1.0

---
+++ Cannon/Assets/scripts/Rasruha.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]//автомат риджит 
+public class Rasruha : MonoBehaviour
+{
+    public float HP_WALLS_ONE,HP_WALLS_TWO,CASTLE_HP;
+    public Rigidbody walls1, walls2, castle,yadroRG;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cbtag")
+        {
+            if (transform.GetComponent<Collider>().tag=="w1")
+            {
+                HP_WALLS_ONE -= yadroRG.mass;
+                if (HP_WALLS_ONE <= 0)
+                {
+
+                    HP_WALLS_ONE = 0;
+                    walls1.isKinematic = false;
+                }
+                
+            }
+            if (transform.GetComponent<Collider>().tag == "w2")
+            {
+                HP_WALLS_TWO -= yadroRG.mass;
+                if (HP_WALLS_TWO <= 0)
+                {
+                    HP_WALLS_TWO = 0;
+                    walls2.isKinematic = false;
+                }
+
+            }
+            if (transform.GetComponent<Collider>().tag == "castlet")
+            {
+                CASTLE_HP -= yadroRG.mass;
+                if (CASTLE_HP <= 0)
+                {
+                    CASTLE_HP = 0;
+                    castle.isKinematic = false;
+                }
+
+            }
+        }
+
+    }
+
+}
---
+++ Cannon/Assets/scripts/cannonController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonController : MonoBehaviour
+{
+    public float mass;
+    public Rigidbody massRG;
+    public int speed;
+    public float friction;
+    public float speed_povorot;
+    float xdegrees, ydegrees;
+    Quaternion fromrotation, torotation;
+    Camera camera;
+    public GameObject cannon_ball;
+    Rigidbody cannon_ball_rb;
+    public Transform shot_pos;
+    public float fire_power;
+    void Start()
+    {
+        camera = Camera.main;
+        massRG.mass = mass;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out hit))//если рэйкаст попал куда то
+        {
+            if (hit.transform.gameObject.tag == "cannon")//если луч попал в объект с тэгом кэннон
+            {
+                if(Input.GetMouseButton(0))//если левая кнопка мыши
+                {
+                    xdegrees -= Input.GetAxis("Mouse Y")*speed*friction;
+                    ydegrees += Input.GetAxis("Mouse X") * speed * friction;
+                    fromrotation = transform.rotation;
+                    torotation = Quaternion.Euler(xdegrees,ydegrees,0);
+                    transform.rotation = Quaternion.Lerp(fromrotation, torotation, Time.deltaTime * speed_povorot);
+                }
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FireCannon();
+        }
+
+    }
+    public void FireCannon()//стреляет пушка
+    {
+        GameObject CannonBallCopy = Instantiate(cannon_ball, shot_pos.position, transform.rotation);
+        cannon_ball_rb = CannonBallCopy.GetComponent<Rigidbody>();//добавляяем к компоненту риджит бади силу
+        cannon_ball_rb.AddForce(transform.forward * fire_power);
+    }
+
+}
